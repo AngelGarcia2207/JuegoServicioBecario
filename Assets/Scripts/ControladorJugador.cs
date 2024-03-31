79440b4633862cc9894442490896c176bef8bd5f
AlejandroA00833118 <102173423+AlejandroA00833118@users.noreply.github.com>
2024-03-30 23:56:12 -0600

    Escalado de bordes

--- Assets/Scripts/ControladorJugador.cs
+++ Assets/Scripts/ControladorJugador.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class ControladorJugador : MonoBehaviour
 {
@@ -17,6 +18,14 @@ public class ControladorJugador : MonoBehaviour
     public float jumpForce;
     public float floorRaycastDistance;
 
+    public float ledgeRaycastHeight;
+    public float ledgeRaycastDistance;
+    public float ledgeClimbingTime;
+    private float ledgeClimbingRemainingTime;
+    private bool climbingDelay = false;
+    public float climbingHorizontalDisplacement;
+    public float climbingVerticalDisplacement;
+
     public Camera mainCamera;
     private Vector3 camForward;
     private Vector3 camRight;
@@ -27,13 +36,12 @@ public class ControladorJugador : MonoBehaviour
     void Start()
     {
         player = GetComponent<CharacterController>();
-        // animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!inmovilizado) {
+        if (!inmovilizado && !climbingDelay) {
             movimientoX = Input.GetAxis("Horizontal");
             movimientoZ = Input.GetAxis("Vertical");
         }
@@ -55,6 +63,22 @@ public class ControladorJugador : MonoBehaviour
         aplicarGravedad();
 
         inputsEspeciales();
+
+        if (!climbingDelay) {
+            climbingDelay = raycastLedge();
+            if (climbingDelay) {
+                ledgeClimbingRemainingTime = ledgeClimbingTime;
+                animator.SetBool("Climbing", true);
+            }
+        }
+        else {
+            ledgeClimbingRemainingTime -= Time.deltaTime;
+            if (ledgeClimbingRemainingTime <= 0f) {
+                ledgeClimb();
+                climbingDelay = false;
+                animator.SetBool("Climbing", false);
+            }
+        }
 
         aplicarAnimacion(movimientoX, movimientoZ);
 
@@ -103,13 +127,18 @@ public class ControladorJugador : MonoBehaviour
     }
 
     void aplicarGravedad() {
-        if (player.isGrounded) {
-            velocidadCaida = -gravedad * 0.1f;
-            direccionJugador.y = velocidadCaida;
+        if (!climbingDelay) {
+            if (player.isGrounded) {
+                velocidadCaida = -gravedad * 0.1f;
+                direccionJugador.y = velocidadCaida;
+            }
+            else {
+                velocidadCaida -= gravedad * Time.deltaTime;
+                direccionJugador.y = velocidadCaida;
+            }
         }
         else {
-            velocidadCaida -= gravedad * Time.deltaTime;
-            direccionJugador.y = velocidadCaida;
+            velocidadCaida = 0;
         }
     }
 
@@ -124,5 +153,34 @@ public class ControladorJugador : MonoBehaviour
         else {
             return false;
         }
+    }
+
+    bool raycastLedge() {
+        Vector3 origin = transform.position + new Vector3(0,ledgeRaycastHeight,0);
+        RaycastHit hit;
+        Vector3 direction = transform.forward;
+
+        if (Physics.Raycast(origin, direction, out hit, ledgeRaycastDistance)) {
+            if (hit.collider.CompareTag("Ledge")) {
+                Debug.DrawRay(origin, direction * ledgeRaycastDistance, Color.white);
+                return true;
+            }
+        }
+        else {
+            Debug.DrawRay(origin, direction * ledgeRaycastDistance, Color.white);
+        }
+        
+        return false;
+    }
+
+    void ledgeClimb() {
+        Vector3 horizontalDirection = new Vector3(transform.forward.x, 0f, transform.forward.z).normalized;
+        Vector3 horizontalDisplacement = horizontalDirection * climbingHorizontalDisplacement;
+
+        Vector3 verticalDisplacement = new Vector3(0f, climbingVerticalDisplacement, 0f);
+
+        Vector3 displacement = horizontalDisplacement + verticalDisplacement;
+
+        transform.position = transform.position + displacement;
     }
 }
