3fa88b79405c78b07f70d3ebbeb9ab24f59e16aa
AlejandroA00833118 <102173423+AlejandroA00833118@users.noreply.github.com>
2024-04-29 00:03:02 -0600

    primera parte segundo nivel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,10 +8,16 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TMP_Text TextoMonedas;
     [SerializeField] private GameObject nivel1Panel;
     [SerializeField] private TMP_Text progresoNivel1Text;
+    [SerializeField] private GameObject nivel2Panel;
+    [SerializeField] private TMP_Text progresoNivel2Text;
+    [SerializeField] private GameObject hotelChico;
+    [SerializeField] private GameObject hotelGrande;
 
     private int monedas;
     public bool primerNivelActivo = false;
     public int personasConvencidas = 0;
+    public bool segundoNivelActivo = false;
+    public int segundoNivelpersonasConvencidas = 0;
 
     public static GameManager Instance { get; private set; }
 
@@ -26,11 +32,23 @@ public class GameManager : MonoBehaviour
         if (primerNivelActivo) {
             nivel1Interfaz();
         }
+        if (segundoNivelActivo) {
+            nivel2Interfaz();
+        }
+        if (segundoNivelpersonasConvencidas == 3) {
+            hotelGrande.SetActive(true);
+            hotelChico.SetActive(false);
+        }
     }
 
     private void nivel1Interfaz() {
         nivel1Panel.SetActive(true);
         progresoNivel1Text.text = personasConvencidas.ToString() + "/5";
+    }
+
+    private void nivel2Interfaz() {
+        nivel2Panel.SetActive(true);
+        progresoNivel2Text.text = segundoNivelpersonasConvencidas.ToString() + "/3";
     }
 
     public void ObtenerMoneda()
---
+++ Assets/Scripts/HotelierDialogue.cs
@@ -0,0 +1,171 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class HotelierDialogue : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject dialogueMark;
+    [SerializeField] private GameObject eventMark;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField] private GameObject dialoguePanelPlayer;
+    [SerializeField] private TMP_Text dialogueTextPlayer;
+    [SerializeField] private float typingTime = 0.05f;
+    [SerializeField, TextArea(4,6)] private string[] dialogueLines;
+    
+    private bool isPlayerInRange;
+    private bool didDialogueStart;
+    private int lineIndex;
+    private bool dialogueEnds = false;
+    private bool availableToTalk = true;
+    private float waitTime = 0.2f;
+    private float lastInputTime;
+
+    private GameObject playerObject;
+    private ControladorJugador playerScript;
+    private Camera mainCamera;
+    private CamaraTerceraPersona cameraScript;
+
+    void Start() {
+
+    }
+
+    void Update()
+    {
+        if (isPlayerInRange && availableToTalk) {
+            if (Input.GetMouseButtonUp(0) && Time.time - lastInputTime > waitTime) {
+                if (!didDialogueStart) {
+                    StartDialogue();
+                }
+                else {
+                    StopAllCoroutines();
+                    dialogueText.text = dialogueLines[lineIndex];
+                }
+                lastInputTime = Time.time;
+            }
+
+            if (dialogueText.text == dialogueLines[lineIndex]) {
+                if (lineIndex == 0) {
+                    lineIndex++;
+                    NextDialogueLine();
+                }
+                
+                if (lineIndex == 1) {
+                    dialogueTextPlayer.text = dialogueLines[3] + '\n' + '\n' + dialogueLines[2];
+                    dialoguePanelPlayer.SetActive(true);
+
+                    if (Input.GetKeyUp(KeyCode.Alpha1) || Input.GetKeyUp(KeyCode.Keypad1)) {
+                        lineIndex = 5;
+                        NextDialogueLine();
+                        dialogueTextPlayer.text = string.Empty;
+                    }
+
+                    if (Input.GetKeyUp(KeyCode.Alpha2) || Input.GetKeyUp(KeyCode.Keypad2)) {
+                        lineIndex = 4;
+                        NextDialogueLine();
+                        dialoguePanelPlayer.SetActive(false);
+                        dialogueText.text = string.Empty;
+                        dialogueEnds = true;
+                    }
+                }
+
+                if (lineIndex == 5) {
+                    dialogueTextPlayer.text = dialogueLines[6];
+
+                    if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1)) {
+                        dialoguePanelPlayer.SetActive(false);
+                        lineIndex = 7;
+                        NextDialogueLine();
+                        dialogueTextPlayer.text = string.Empty;
+                        dialogueEnds = true;
+                        GameManager.Instance.segundoNivelActivo = true;
+                    }
+                }
+
+                if (Input.GetMouseButtonDown(0) && dialogueEnds == true && Time.time - lastInputTime > waitTime) {
+                    NextDialogueLine();
+                    lastInputTime = Time.time;
+
+                    if (lineIndex == 7) {
+                        availableToTalk = false;
+                        dialogueMark.SetActive(false);
+                    }
+                }
+            }
+        }
+        else if(!GameManager.Instance.segundoNivelActivo){
+            eventMark.SetActive(true);
+        }
+        else{
+            eventMark.SetActive(false);
+        }
+    }
+
+    private void StartDialogue() {
+        didDialogueStart = true;
+        dialoguePanel.SetActive(true);
+        dialogueMark.SetActive(false);
+        lineIndex = 0;
+
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        playerScript = playerObject.GetComponent<ControladorJugador>();
+        mainCamera = Camera.main;
+        cameraScript = mainCamera.GetComponent<CamaraTerceraPersona>();
+        
+        Vector3 playerDirection = playerObject.transform.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(new Vector3(playerDirection.x, 0, playerDirection.z));
+
+        playerScript.hablando = true;
+        cameraScript.inmovilizado = true;
+
+        StartCoroutine(ShowLine());
+    }
+
+    private void NextDialogueLine() {
+        if (!dialogueEnds) {
+            StartCoroutine(ShowLine());
+        }
+        else {
+            didDialogueStart = false;
+            dialoguePanel.SetActive(false);
+            dialoguePanelPlayer.SetActive(false);
+            dialogueMark.SetActive(true);
+            animator.SetBool("Talking", false);
+            dialogueEnds = false;
+
+            ControladorJugador playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<ControladorJugador>();
+            playerScript.hablando = false;
+            cameraScript.inmovilizado = false;
+        }
+    }
+
+    private IEnumerator ShowLine() {
+        if (lineIndex > 0) {
+            animator.SetBool("Talking", true);
+        }
+
+        dialogueText.text = string.Empty;
+
+        foreach(char ch in dialogueLines[lineIndex]) {
+            dialogueText.text += ch;
+            yield return new WaitForSeconds(typingTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision) {
+        if (collision.gameObject.CompareTag("Player") && availableToTalk && !GameManager.Instance.segundoNivelActivo) {
+            isPlayerInRange = true;
+            dialogueMark.SetActive(true);
+            eventMark.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit(Collider collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            isPlayerInRange = false;
+            dialogueMark.SetActive(false);
+            eventMark.SetActive(true);
+        }
+    }
+}
