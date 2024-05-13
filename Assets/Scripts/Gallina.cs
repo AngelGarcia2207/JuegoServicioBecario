d9fbf9c9fbd289a0d6d9d5751615c1858842bf80
AlejandroA00833118 <102173423+AlejandroA00833118@users.noreply.github.com>
2024-05-12 18:04:15 -0600

    modelo de la gallina y textura

--- Assets/Scripts/Gallina.cs
+++ Assets/Scripts/Gallina.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class Gallina : MonoBehaviour
 {
     public Transform[] waypoints;
-    public float speed = 5.0f;
+    public float speed = 30.0f;
+    public float verticalShift = 0.3f;
+    public float bounceSpeed = 0.05f;
 
     private int currentWaypointIndex = 0;
+    private float originalY;
+    private float targetY;
+    private bool movingUp = true;
+
+    void Start() {
+        originalY = transform.position.y;
+        StartCoroutine(bounceCoroutine());
+    }
 
     void Update()
     {
@@ -19,12 +29,33 @@ public class Gallina : MonoBehaviour
 
         transform.position += direction * speed * Time.deltaTime;
 
+        transform.rotation = Quaternion.LookRotation(direction);
+
         if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].position) < 0.1f) {
             currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
         }
     }
 
-    private void OnTriggerEnter(Collider collision) {
+    IEnumerator bounceCoroutine() {
+        while (true) {
+            if (movingUp) {
+            targetY = transform.position.y + verticalShift;
+            }
+            else {
+                targetY = originalY;
+            }
+
+            while (Mathf.Abs(targetY - transform.position.y) > 0.01f) {
+                float newY = Mathf.MoveTowards(transform.position.y, targetY, bounceSpeed * Time.deltaTime);
+                transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+                yield return null;
+            }
+
+            movingUp = !movingUp;
+        }
+    }
+
+    void OnTriggerEnter(Collider collision) {
         if (collision.gameObject.CompareTag("Player") && GameManager.Instance.segundoNivelActivo) {
             // Alguna transición que diga "Has capturado a la gallina exitosamente"
 
