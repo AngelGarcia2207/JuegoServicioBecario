3c5d61f32f88dda40fdad2606998c5286c508e48
Angel Garcia <102173690+AngelGarcia2207@users.noreply.github.com>
2024-05-21 12:11:27 -0600

    Skybox y fixes

---
+++ Assets/Scripts/LibroFinal2.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LibroFinal2 : MonoBehaviour
+{
+    [SerializeField] private Animator transicion;
+    [SerializeField] private Animator player;
+    [SerializeField] private Animator libro;
+    [SerializeField] private GameObject panelProgreso;
+    [SerializeField] private GameObject panelVictoria;
+
+    private ControladorJugador playerScript;
+    private Camera mainCamera;
+    private CamaraTerceraPersona cameraScript;
+    private bool collided;
+
+    void Start()
+    {
+        libro.speed = 1f;
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<ControladorJugador>();
+        mainCamera = Camera.main;
+        cameraScript = mainCamera.GetComponent<CamaraTerceraPersona>();
+    }
+
+    private void OnTriggerEnter(Collider collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            collided = true;
+            playerScript.inmovilizado = true;
+            cameraScript.inmovilizado = true;
+            player.speed = 0f;
+            libro.speed = 0f;
+            panelProgreso.SetActive(false);
+            transicion.speed = 0.5f;
+            StartCoroutine("fin");
+            transicion.SetTrigger("Fade");
+        }
+    }
+
+    IEnumerator fin(){
+        yield return new WaitForSeconds(4f);
+        panelVictoria.SetActive(true);
+    }
+}
