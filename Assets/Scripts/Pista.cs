7c48a8e5ba68d512fe7d0fd6665afa57ff9bd839
AlejandroA00833118 <a00833118@itesm.mx>
2024-05-24 20:50:58 -0600

    avance nivel 5

---
+++ Assets/Scripts/Pista.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Pista : MonoBehaviour
+{
+    [SerializeField] private GameObject dialoguePanelPlayer;
+    [SerializeField] private TMP_Text dialogueTextPlayer;
+    [SerializeField] private float typingTime = 0.05f;
+    [SerializeField, TextArea(4,6)] private string[] dialogueLines;
+    
+    private bool isPlayerInRange;
+    private bool didDialogueStart;
+    private int lineIndex;
+    private bool availableToTalk = true;
+    private bool taskStarted = false;
+
+    private GameObject playerObject;
+    private ControladorJugador playerScript;
+
+    void Update()
+    {
+        if (isPlayerInRange && availableToTalk) {
+            if (!didDialogueStart) {
+                StartDialogue();
+            }
+            else if (dialogueTextPlayer.text == dialogueLines[lineIndex] && Input.GetMouseButtonUp(0)) {
+                NextDialogueLine();
+            }
+            else if (Input.GetMouseButtonUp(0)) {
+                StopAllCoroutines();
+                dialogueTextPlayer.text = dialogueLines[lineIndex];
+            }
+        }
+    }
+
+    private void StartDialogue() {
+        didDialogueStart = true;
+        dialoguePanelPlayer.SetActive(true);
+
+        lineIndex = 0;
+
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        playerScript = playerObject.GetComponent<ControladorJugador>();
+
+        playerScript.hablando = true;
+
+        StartCoroutine(ShowLine());
+    }
+
+    private void NextDialogueLine() {
+        lineIndex++;
+
+        if (lineIndex < dialogueLines.Length) {
+            StartCoroutine(ShowLine());
+        }
+        else {
+            didDialogueStart = false;
+            availableToTalk = false;
+            dialoguePanelPlayer.SetActive(false);
+
+            ControladorJugador playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<ControladorJugador>();
+            playerScript.hablando = false;
+        }
+    }
+
+    private IEnumerator ShowLine() {
+        dialogueTextPlayer.text = string.Empty;
+
+        foreach(char ch in dialogueLines[lineIndex]) {
+            dialogueTextPlayer.text += ch;
+            yield return new WaitForSeconds(typingTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision) {
+        if (collision.gameObject.CompareTag("Player") && availableToTalk) {
+            isPlayerInRange = true;
+        }
+    }
+}
